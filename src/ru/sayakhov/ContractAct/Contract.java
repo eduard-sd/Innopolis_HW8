d8abd9521eba8690c756167d8c79ddd51667c4e6
eduard-sd <pr-spektr@yandex.ru>
2018-09-14 18:29:46 +0300

    refactoring convector code and calculater

---
+++ src/ru/sayakhov/ContractAct/Contract.java
@@ -0,0 +1,33 @@
+package ru.sayakhov.ContractAct;
+
+
+public class Сontract {
+    private int number;
+    private String date;
+    private String[] productList;
+
+    public  int getNumber() {
+        return number;
+    }
+
+    public  void setNumber(int number) {
+        this.number = number;
+    }
+
+    public  String getDate() {
+        return date;
+    }
+
+    public  void setDate(String date) {
+        this.date = date;
+    }
+
+    public  String[] getProductList() {
+        return productList;
+    }
+
+    public  void setProductList(String[] productList) {
+        this.productList = productList;
+    }
+
+}
